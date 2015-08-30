36117d3919bd873081a0df191d5d5e2359b8baa6
Hao Zhu <haozhu@hsl.harvard.edu>
2015-08-30 16:23:53 -0400

    Update test file for simple_summary_categorical to improve test coverage.

--- tests/testthat/test-simple_summary_categorical.R
+++ tests/testthat/test-simple_summary_categorical.R
@@ -3,7 +3,7 @@ context("Summary_Categorical")
 
 df <- mtcars %>% group_by(am) %>% select(gear)
 df2 <- mtcars %>% group_by(am) %>% select(gear, carb)
-
+df3 <- mtcars %>% select(gear)
 test_that("count_percentage works (when adjusting N and round.N)", {
   expect_result_original <- data.frame(x = c(3,4,5),
                                        freq = c(15L, 12L, 5L),
@@ -40,9 +40,18 @@ test_that("count_percentage_ can also analyze 2 or more variables", {
   expect_equal(dim(count_percentage_(df2)), expected = c(13,5))
 })
 
-test_that("simple_summary_categorical can rename the variables", {
+test_that("simple_summary_categorical can work without grouping data", {
+  expect_equal(names(simple_summary_categorical(df3)), expected = c("x", "freq", "Percentage"))
+  expect_equal(names(simple_summary_categorical(df3, n=T)), expected = c("x", "N", "freq", "Percentage"))
+  expect_equal(simple_summary_categorical(df3)[2], expected = structure(list(freq = c(15L, 12L,5L)), class = c("tbl_df", "data.frame"), row.names = 1:3))
+  expect_equal(simple_summary_categorical(df3, n=T)[2:3], expected = structure(list(N=c(32L, 32L, 32L), freq = c(15L, 12L,5L)), class = c("tbl_df", "data.frame"), row.names = 1:3))
+})
+
+test_that("simple_summary_categorical can rename the variables for data with grouping info", {
   expect_equal(names(simple_summary_categorical(df2))[2], expected = c("x"))
+  expect_equal(names(simple_summary_categorical(df2, n=T))[2:3], expected = c("x", "N"))
   expect_equivalent(as.character(simple_summary_categorical(df2)[2, 2]), expected = c("gear_4"))
+  expect_equivalent(as.character(simple_summary_categorical(df2, n=T)[2, 2:3]), expected = c("gear_4", "19"))
 })
 
 test_that("simple_summary_categorical can export n.group and n.var", {
